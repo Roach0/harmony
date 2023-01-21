1cf9d139566a922e47baab7a74156ba08192f781
vicsan2 <victormsanchez2222@gmail.com>
2023-01-20 20:09:59 -0600

    url fix

--- app/helpers/urls.ts
+++ app/helpers/urls.ts
@@ -1,7 +1,7 @@
 export const discordApiUrl = "https://discord.com/api";
 
 export const discordAuthUrl = (clientId: string, hostUrl?: string) =>
-  `${discordApiUrl}/oauth2/authorize?client_id=${clientId}&redirect_uri=http%3A%2F%2F${hostUrl}%3A3000%2Fapp&response_type=code&scope=identify`;
+  `${discordApiUrl}/oauth2/authorize?client_id=${clientId}&redirect_uri=${hostUrl}%2Fapp&response_type=code&scope=identify`;
 
 export const discordPhotoUrl = (discordId: string, avatar: string) =>
   `https://cdn.discordapp.com/avatars/${discordId}/${avatar}.png`;
